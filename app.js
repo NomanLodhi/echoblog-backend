62c5bdfb04d70ab1e3e8ec626a16f5cac6ede460
NomanLodhi <110075584+NomanLodhi@users.noreply.github.com>
2025-06-27 04:42:35 -0700

    changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const comments=require('./routes/comment.route')
 app.use(express.json());
 app.use('/files',express.static(path.join(__dirname,'files')))
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'https://echoblog-theta.vercel.app/'
   }));
 app.use('/api/v1/users',user);
 app.use('/api/v1/admin',admin);
