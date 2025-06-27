c0668b59b82964dddfe5050fdcc4d35ee8c6f1b0
NomanLodhi <110075584+NomanLodhi@users.noreply.github.com>
2025-06-27 04:16:54 -0700

    changes

--- config/db.js
+++ config/db.js
@@ -4,8 +4,10 @@ const db_name=process.env.DB_NAME;
 const db_username=process.env.DB_USERNAME;
 const db_password=process.env.DB_PASSWORD;
 const db_host=process.env.DB_HOST;
+const db_port=process.env.DB_PORT;
 const sequelize=new Sequelize(db_name,db_username,db_password,{
     host:db_host,
+    port:db_port,
     dialect:'mysql'
 })
 module.exports=sequelize
